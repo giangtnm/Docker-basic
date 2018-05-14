de2f60e929514f7d8ae9625b6f6e315e484ffede
Giang 8E31 <14520231@gm.uit.edu.vn>
2018-05-14 14:16:44 +0700

    change socket.gethostname() to /etc/hostname

--- docker-swarm/app.py
+++ docker-swarm/app.py
@@ -3,12 +3,14 @@ from redis import Redis
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+hostname = open('/etc/hostname', 'r')
+host = hostname.read()
 
 @app.route('/')
 def hello():
     #count = redis.incr('hits')
     #return 'Hello World! I have been seen {} times.\n'.format(count)
-    print socket.gethostname()
+    return host
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
