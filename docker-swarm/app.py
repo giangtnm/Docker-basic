f9d4d32bd51d8de2312bfbe8ab023a6eceaddc13
Giang 8E31 <14520231@gm.uit.edu.vn>
2018-05-14 11:33:13 +0700

    socket.gethostname()

--- docker-swarm/app.py
+++ docker-swarm/app.py
@@ -6,8 +6,9 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    #count = redis.incr('hits')
+    #return 'Hello World! I have been seen {} times.\n'.format(count)
+    print socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
